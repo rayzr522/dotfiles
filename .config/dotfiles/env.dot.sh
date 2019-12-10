6d2011a7d300948f29283936afcc4e20229bf240
Rayzr522 <rayzr522@gmail.com>
2019-12-10 09:37:28 -0500

    [zsh:env] Add current directory's node_modules/.bin

--- .config/dotfiles/env.dot.sh
+++ .config/dotfiles/env.dot.sh
@@ -3,6 +3,6 @@
 # Folder for all personal scripts.
 export SCRIPTS="$HOME/.bin"
 
-export PATH="$PATH:$SCRIPTS:.:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin"
+export PATH="$PATH:$SCRIPTS:.:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin:./node_modules/.bin"
 
 export SRVR="$HOME/Servers/Minecraft/paper-server"
