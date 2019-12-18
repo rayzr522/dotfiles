0cd4e35a91f0fa4363640cca52ee631e8c61f9c0
Rayzr522 <rayzr522@gmail.com>
2019-12-17 19:38:54 -0500

    [zsh:env] Proper EDITOR and VISUAL vars

--- .config/dotfiles/env.dot.sh
+++ .config/dotfiles/env.dot.sh
@@ -5,4 +5,8 @@ export SCRIPTS="$HOME/.bin"
 
 export PATH="$PATH:$SCRIPTS:.:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin:./node_modules/.bin"
 
+# Aliases will handle translating from vim to nvim
+export EDITOR=vim
+export VISUAL="$EDITOR"
+
 export SRVR="$HOME/Servers/Minecraft/paper-server"
