450c9d6ab409cd42af5a083439194ee91a2710ad
Rayzr522 <rayzr522@gmail.com>
2020-01-07 00:48:35 -0500

    [zsh:env] Fix $EDITOR & $VISUAL to use nvim

--- .config/dotfiles/env.dot.sh
+++ .config/dotfiles/env.dot.sh
@@ -5,8 +5,7 @@ export SCRIPTS="$HOME/.bin"
 
 export PATH="$PATH:$SCRIPTS:.:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin:./node_modules/.bin"
 
-# Aliases will handle translating from vim to nvim
-export EDITOR=vim
+export EDITOR=nvim
 export VISUAL="$EDITOR"
 
 export SRVR="$HOME/Servers/Minecraft/paper-server"
