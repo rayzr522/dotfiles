25a81711f3a14f815e10ff2e52174cbc0f73f7e9
Rayzr522 <rayzr522@gmail.com>
2020-06-18 15:45:40 -0400

    [zsh:aliases] Add vim folding keys

--- .config/dotfiles/aliases.dot.sh
+++ .config/dotfiles/aliases.dot.sh
@@ -5,25 +5,28 @@ unalias o 2>/dev/null
 
 alias rs='source ~/.zshrc;'
 
-# Navigation
+# Navigation {{{
 alias ~='cd ~'
 alias h='cd ~'
 alias ..='cd ..'
 alias gh='cd ~/GitHub'
 alias dt='cd ~/Desktop'
 alias dl='cd ~/Downloads'
+# }}}
 
-# Confirmation
+# Confirmation {{{
 alias mv='mv -i'
 alias cp='cp -i'
 alias ln='ln -i'
+# }}}
 
-# Server / Network
+# Server / Network {{{
 alias header='curl -I'
 alias headerc='curl -I --compress'
 alias get='wget --content-disposition'
+# }}}
 
-# Fix vi/vim (and by vi/vim I mean me because I am the one who forgets)
+# Fix vi/vim (and by vi/vim I mean me because I am the one who forgets) {{{
 alias vi='nvim'
 alias vim='nvim'
 # shorthand because i Am LaZy
@@ -32,12 +35,14 @@ alias v='nvim'
 # I'm addicted
 alias :wq='exit'
 alias :q!='exit'
+# }}}
 
-# Sane defaults
+# Sane defaults {{{
 alias grep='grep --exclude-dir=node_modules'
 alias ls='ls --color=auto'
+# }}}
 
-# Git
+# Git {{{
 alias gs='git status -s'
 alias gl='git log'
 alias gls='git log --pretty=oneline'
@@ -58,8 +63,9 @@ alias gbD='gbd -D'
 alias grh='git reset --hard'
 alias gss='git stash'
 alias gsp='git stash pop'
-
-# NPM
+# }}}
+ 
+# NPM {{{
 alias npr='npm run'
 
 alias nps='npr start'
@@ -69,34 +75,42 @@ alias npl='npr lint'
 
 alias npi='npm install'
 alias npci='npm ci'
+# }}}
 
-# DB
+# DB {{{
 alias mongo='mongo --quiet'
+# }}}
 
-# Misc optional tool shortcuts
+# Misc optional tool shortcuts {{{
 if command -v shellcheck >/dev/null; then
     alias sc='shellcheck '
 fi
+# }}}
 
-# CI/CD
+# CI/CD {{{
 alias maven-deploy='mvn -DaltDeploymentRepository=local-repo::default::file://$HOME/GitHub/maven-repo clean package deploy'
+# }}}
 
-# Quick launchers
+# Quick launchers {{{
 alias vps-stats='ssh vps "TERM=$TERM" ./stats.sh'
+# }}}
 
-# Info commands
+# Info commands {{{
 alias sysinfo='clear;neofetch;read -sk 1;echo'
 alias song='echo "$(playerctl metadata artist) - $(playerctl metadata title)"'
 alias localip='ip addr show | grep "inet " | grep -v "127.0.0.1" | head -n1 | awk '\''{print $2}'\'' | cut -d/ -f1'
 alias lastcommit='git log -n 1 -pretty=format:"%H"'
 alias md-view='node ~/GitHub/markdown-parser '
 alias diskstat='df -h | grep -E '\''^/dev'\'' | sort | awk '\''{ print $1 ": " $3 "/" $2 " used (" $5 ")" }'\'''
+# }}}
 
-# Templating commands
+# Templating commands {{{
 alias joinme='echo -e "\n## Join Me\n[![Discord Badge](https://github.com/Rayzr522/ProjectResources/raw/master/RayzrDev/badge-small.png)](https://discord.io/rayzrdevofficial)" >> '
+# }}}
 
-# Joke commands
+# Joke commands {{{
 alias wininfo='xprop -id "$(xwininfo | grep -oE "0x[0-9a-f]{7}")"'
 alias boopboom='kill "$(wininfo | grep _NET_WM_PID | cut -d" " -f3)"'
 alias boxify='cowsay -W "$(( $(tput cols) - 3))" | head -n -5'
 alias chill='screen -dmS music mpsyt /lunacy chillout, "$(( RANDOM % 20 ))"'
+# }}}
