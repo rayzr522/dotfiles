56c61634bdd4ca7b9ad20efcdf466962ff5f8e46
Rayzr522 <rayzr522@gmail.com>
2021-06-16 15:10:09 -0400

    [zsh:env] configure npm/node by default

--- .config/dotfiles/env.dot.sh
+++ .config/dotfiles/env.dot.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Folder for all personal scripts.
+# Important directories
 export SCRIPTS="$HOME/.bin"
-
-export PATH="$PATH:$SCRIPTS:.:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin:./node_modules/.bin"
-
-export EDITOR=nvim
-export VISUAL="$EDITOR"
-
 export SRVR="$HOME/Servers/Minecraft/paper-server"
 
-export SUDO_ASKPASS="$HOME/.bin/askpass-secret-tool"
+export PATH="$PATH:$SCRIPTS:$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin:./node_modules/.bin"
+export EDITOR=nvim
+export VISUAL="$EDITOR"
+export SUDO_ASKPASS="$SCRIPTS/askpass-secret-tool"
+
+export NPM_PACKAGES="$HOME/.cache/npm-packages"
+export PATH="$NPM_PACKAGES/bin:$PATH"
+export MANPATH="$NPM_PACKAGES/share/man:${MANPATH:-$(manpath)}"
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+export N_PREFIX="$NPM_PACKAGES"
