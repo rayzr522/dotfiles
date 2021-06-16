573ac7da4e511df381713b6f6bc77adf2f7e7750
Rayzr522 <rayzr522@gmail.com>
2021-06-16 15:24:06 -0400

    [zsh:env] dont re-export env vars

--- .config/dotfiles/env.dot.sh
+++ .config/dotfiles/env.dot.sh
@@ -3,14 +3,19 @@
 # Important directories
 export SCRIPTS="$HOME/.bin"
 export SRVR="$HOME/Servers/Minecraft/paper-server"
+export NPM_PACKAGES="$HOME/.cache/npm-packages"
+export N_PREFIX="$NPM_PACKAGES"
 
-export PATH="$PATH:$SCRIPTS:$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/go/bin:./node_modules/.bin"
+# Configure discovery paths
+export PATH="$HOME/.local/bin:$SCRIPTS:$NPM_PACKAGES/bin:$HOME/go/bin:$_ORIG_PATH:./node_modules/.bin"
+if ! echo "$MANPATH" | grep -q "$NPM_PACKAGES/share/man:"; then
+    export MANPATH="$NPM_PACKAGES/share/man:${MANPATH:-$(manpath)}"
+fi
+if ! echo "$NODE_PATH" | grep -q "$NPM_PACKAGES/lib/node_modules:"; then
+    export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+fi
+
+# Configure tools
 export EDITOR=nvim
 export VISUAL="$EDITOR"
 export SUDO_ASKPASS="$SCRIPTS/askpass-secret-tool"
-
-export NPM_PACKAGES="$HOME/.cache/npm-packages"
-export PATH="$NPM_PACKAGES/bin:$PATH"
-export MANPATH="$NPM_PACKAGES/share/man:${MANPATH:-$(manpath)}"
-export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-export N_PREFIX="$NPM_PACKAGES"
