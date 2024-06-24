21939a59ec87a0e25e79f7f224822ae2cb990736
rayzr522 <rayzr522@gmail.com>
2024-06-24 12:47:21 -0400

    [zsh] clean up functions

--- zsh/.config/zsh/init/functions.zsh
+++ zsh/.config/zsh/init/functions.zsh
@@ -1,60 +1,10 @@
-# Sorts STDIN by length.
-function sortlen {
-    while read -r line; do echo "${#line} $line"; done |\
-        sort -n | cut -d' ' -f2-
-}
-
-# Reverses STDIN.
-function reverse {
-    OUT=""
-
-    for word in "$@"; do
-        for char in $(echo "$word" | sed 's/./\0\n/g'); do
-            OUT="$char$OUT"
-        done
-        OUT=" $OUT"
-    done
-
-    echo "$OUT" | cut -c2-
-}
-
-# Changelog command
-function changelog {
-    if [[ $# -lt 2 ]]; then
-        echo "Usage: changelog <tag from> <tag to>" >&2
-        return 1
-    fi
-
-    printf "### Changelog\n\n"
-
-    git log --format='* %H - %s' "$1".."$2"
-
-    printf "\nEnjoy!\n\n- rayzr :computer:\n"
-}
-
-# Changes the online-mode of the server.properties file in the current directory to true or false.
-function minecraft-online-mode {
-    if [ ! -f server.properties ]; then
-        echo "No server.properties file could be found." >&1
-        return 1
-    fi
-
-    if ! echo "$1" | grep -qE 'true|false'; then
-        echo "Usage: minecraft-online-mode <true|false>" >&1
-        return 1
-    fi
-
-    sed -i "s/online-mode=\\(true\\|false\\)/online-mode=$1/" server.properties
-    echo "online-mode set to '$1'"
-}
-
-# Edits the given dotfile and then re-sources it.
+# edits the given dotfile and then re-sources it
 function dfe {
     local dotfile="$HOME/.config/zsh/init/$1"
     nvim "$dotfile" && source "$dotfile"
 }
 
-# Opens the given path or the current directory.
+# opens path in GUI (supporting piping)
 function o {
     local dir="$@"
     if [[ -z "$dir" ]] && [[ ! -t 0 ]]; then
@@ -62,20 +12,10 @@ function o {
         dir="$line"
     fi
 
-    "$SCRIPTS/open" "${dir:-.}" 1&>/dev/null
-    return $?
+    open "${dir:-.}" 1&>/dev/null
 }
 
-# Edits a file in Moeditor in the background.
-function md {
-    if ! command -v moeditor >/dev/null; then
-        echo "Moeditor was not installed or is not in the PATH!" >&2
-        return 1
-    fi
-
-    (moeditor "$@" &)
-}
-
+# locates a file
 function loc {
     if [[ $# -lt 1 ]]; then
         echo "Usage: loc <query> [folder]" >&2
@@ -88,6 +28,7 @@ function loc {
     rg --color always --line-number --ignore-case "$query" "$dir" | less -R
 }
 
+# finds a font
 function fc-find {
     if [[ $# -lt 1 ]]; then
         echo "Usage: fc-find <name>" >&2
@@ -104,58 +45,8 @@ function fc-find {
         | sed -e 's/^ //g' -e 's/,/, /g'
 }
 
-function gm {
-    if [[ $# -lt 1 ]]; then
-        echo "Usage: gm <name> [--init]" >&2
-        return 1
-    fi
-
-    local gm_dir="$HOME/GitHub/.mirror"
-
-    if [[ ! -d "$gm_dir" ]]; then
-        echo "Creating git mirror dir..."
-        mkdir -p "$gm_dir"
-    fi
-
-    local mirror_path="$gm_dir/$1.git"
-
-    if [[ "$2" = "--init" ]]; then
-        if [[ -d "$mirror_path" ]]; then
-            echo "That mirror already exists!" >&2
-            return 1
-        fi
-
-        echo "Initializing git mirror '$1'..."
-        mkdir -p "$mirror_path"
-        git --git-dir "$mirror_path" init --bare
-        echo "Done!"
-    elif [[ -d "$mirror_path" ]]; then
-        echo "Cloning git mirror '$1'..."
-        git clone "$mirror_path" "$1"
-    else
-        echo "That git mirror does not exist!" >&2
-        return 1
-    fi
-}
-
 function sizes {
     du -sh "$@" | sort -h
-}
-
-function set-gnome-lockscreen {
-    gsettings set org.gnome.desktop.screensaver picture-uri "file://$(realpath $1)"
-}
-
-function github {
-    local url
-    url="$(giturl "$@")"
-    local ret=$?
-
-    if [[ $ret != 0 ]]; then
-        return $ret
-    fi
-
-    o "$url"
 }
 
 function convert-to-mp4 {
@@ -167,20 +58,21 @@ function convert-to-mp4 {
     ffmpeg -i "$1" -vcodec h264_nvenc -crf 30 -acodec copy "$2"
 }
 
+# creates an encrypted backup of the given path
 function bak {
     if [[ $# -lt 1 ]]; then
         echo "usage: bak <path>" >&2
         return 1
     fi
 
-    mkdir -p ~/.bak
-    if [[ ! -e "$1" ]]; then
-        echo "'$1' does not exist" >&2
+    if [[ -z "$BAK_GPG_ID" ]]; then
+        echo "BAK_GPG_ID not configured" >&2
         return 1
     fi
 
-    if [[ -z "$BAK_GPG_ID" ]]; then
-        echo "BAK_GPG_ID not configured" >&2
+    mkdir -p ~/.bak
+    if [[ ! -e "$1" ]]; then
+        echo "'$1' does not exist" >&2
         return 1
     fi
 
