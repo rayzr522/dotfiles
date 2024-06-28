5793e70139562a6af97a8a52c9c2182319791953
rayzr522 <rayzr522@gmail.com>
2024-06-28 05:45:44 -0400

    [nvim] add catgoose/vue-goto-definition.nvim

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -19,6 +19,33 @@ return {
       })
     end
   },
+  {
+    "catgoose/vue-goto-definition.nvim",
+    event = "BufReadPre",
+    opts = {
+      filters = {
+        auto_imports = true,
+        auto_components = true,
+        import_same_file = true,
+        declaration = true,
+        duplicate_filename = true,
+      },
+      filetypes = { "vue", "typescript" },
+      detection = {
+        nuxt = function()
+          return vim.fn.glob(".nuxt/") ~= ""
+        end,
+        vue3 = function()
+          return vim.fn.filereadable("vite.config.ts") == 1 or vim.fn.filereadable("src/App.vue") == 1
+        end,
+        priority = { "nuxt", "vue3" },
+      },
+      lsp = {
+        override_definition = true, -- override vim.lsp.buf.definition
+      },
+      debounce = 0
+    }
+  },
   -- {
   --   "danymat/neogen",
   --   config = function()
